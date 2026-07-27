d741a12de593eef5750d885cbf9fcbaf7c010a24
alex <907785012@qq.com>
2026-07-27 23:07:18 +0800

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require('../../assets/images/icon.png')}
+        style={{ width: 200, height: 200, borderRadius: 20 }}
+      />
+      <Link href={'/about'}>Go To About</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
