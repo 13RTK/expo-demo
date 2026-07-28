ead1d3bd81c686ca9247d8b1e8ab7d1b19b5bff6
alex <907785012@qq.com>
2026-07-28 19:59:35 +0800

    complete nativewind setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,13 @@
-import { Image } from 'expo-image';
 import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Image
-        source={require('../../assets/images/icon.png')}
-        style={{ width: 200, height: 200, borderRadius: 20 }}
-      />
+      <Text className='text-red-800 underline text-4xl'>
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
+
       <Link href={'/about'}>Go To About</Link>
     </View>
   );
